dc23eb7346328ba1c29396a854e3ed58513ae021
sebvrag <sebastien.v@cerca.io>
2025-02-20 09:39:15 +0100

    Update clubs.js

--- data/clubs.js
+++ data/clubs.js
@@ -1,13 +1,163 @@
 export const clubs = [
+  // Ligue 1
   {
-    "name": "Manchester City",
-    "country": "England",
-    "transfermarktUrl": "https://www.transfermarkt.com/manchester-city/startseite/verein/281"
+    "name": "Paris Saint-Germain",
+    "country": "France",
+    "transfermarktUrl": "https://www.transfermarkt.com/paris-saint-germain/startseite/verein/583"
   },
   {
-    "name": "Arsenal FC",
-    "country": "England",
-    "transfermarktUrl": "https://www.transfermarkt.com/arsenal-fc/startseite/verein/11"
-  }
-  // Note: This is just a sample, the actual data will be populated by the scraper
-];
+    "name": "AS Monaco",
+    "country": "France",
+    "transfermarktUrl": "https://www.transfermarkt.com/as-monaco/startseite/verein/162"
+  },
+  {
+    "name": "LOSC Lille",
+    "country": "France",
+    "transfermarktUrl": "https://www.transfermarkt.com/losc-lille/startseite/verein/1082"
+  },
+  {
+    "name": "Olympique Lyonnais",
+    "country": "France",
+    "transfermarktUrl": "https://www.transfermarkt.com/olympique-lyonnais/startseite/verein/1041"
+  },
+  {
+    "name": "Olympique de Marseille",
+    "country": "France",
+    "transfermarktUrl": "https://www.transfermarkt.com/olympique-de-marseille/startseite/verein/244"
+  },
+  {
+    "name": "Stade Rennais FC",
+    "country": "France",
+    "transfermarktUrl": "https://www.transfermarkt.com/stade-rennais-fc/startseite/verein/273"
+  },
+  {
+    "name": "OGC Nice",
+    "country": "France",
+    "transfermarktUrl": "https://www.transfermarkt.com/ogc-nice/startseite/verein/417"
+  },
+  {
+    "name": "RC Strasbourg Alsace",
+    "country": "France",
+    "transfermarktUrl": "https://www.transfermarkt.com/rc-strasbourg-alsace/startseite/verein/667"
+  },
+  {
+    "name": "Montpellier HSC",
+    "country": "France",
+    "transfermarktUrl": "https://www.transfermarkt.com/montpellier-hsc/startseite/verein/969"
+  },
+  {
+    "name": "FC Nantes",
+    "country": "France",
+    "transfermarktUrl": "https://www.transfermarkt.com/fc-nantes/startseite/verein/995"
+  },
+  {
+    "name": "Stade de Reims",
+    "country": "France",
+    "transfermarktUrl": "https://www.transfermarkt.com/stade-de-reims/startseite/verein/142"
+  },
+  {
+    "name": "FC Lorient",
+    "country": "France",
+    "transfermarktUrl": "https://www.transfermarkt.com/fc-lorient/startseite/verein/1217"
+  },
+  {
+    "name": "Toulouse FC",
+    "country": "France",
+    "transfermarktUrl": "https://www.transfermarkt.com/toulouse-fc/startseite/verein/415"
+  },
+  {
+    "name": "Stade Brestois 29",
+    "country": "France",
+    "transfermarktUrl": "https://www.transfermarkt.com/stade-brestois-29/startseite/verein/1234"
+  },
+  {
+    "name": "Angers SCO",
+    "country": "France",
+    "transfermarktUrl": "https://www.transfermarkt.com/angers-sco/startseite/verein/1420"
+  },
+  {
+    "name": "FC Metz",
+    "country": "France",
+    "transfermarktUrl": "https://www.transfermarkt.com/fc-metz/startseite/verein/326"
+  },
+  {
+    "name": "Clermont Foot 63",
+    "country": "France",
+    "transfermarktUrl": "https://www.transfermarkt.com/clermont-foot-63/startseite/verein/1310"
+  },
+  {
+    "name": "ESTAC Troyes",
+    "country": "France",
+    "transfermarktUrl": "https://www.transfermarkt.com/estac-troyes/startseite/verein/1108"
+  },
+  {
+    "name": "RC Lens",
+    "country": "France",
+    "transfermarktUrl": "https://www.transfermarkt.com/rc-lens/startseite/verein/273"
+  },
+  {
+    "name": "FC Girondins de Bordeaux",
+    "country": "France",
+    "transfermarktUrl": "https://www.transfermarkt.com/fc-girondins-de-bordeaux/startseite/verein/40"
+  },
+  // Ligue 2
+  {
+    "name": "Le Havre AC",
+    "country": "France",
+    "transfermarktUrl": "https://www.transfermarkt.com/le-havre-ac/startseite/verein/273"
+  },
+  {
+    "name": "AJ Auxerre",
+    "country": "France",
+    "transfermarktUrl": "https://www.transfermarkt.com/aj-auxerre/startseite/verein/292"
+  },
+  {
+    "name": "SM Caen",
+    "country": "France",
+    "transfermarktUrl": "https://www.transfermarkt.com/sm-caen/startseite/verein/317"
+  },
+  {
+    "name": "FC Sochaux-Montbéliard",
+    "country": "France",
+    "transfermarktUrl": "https://www.transfermarkt.com/fc-sochaux-montbeliard/startseite/verein/610"
+  },
+  {
+    "name": "AS Nancy Lorraine",
+    "country": "France",
+    "transfermarktUrl": "https://www.transfermarkt.com/as-nancy-lorraine/startseite/verein/273"
+  },
+  {
+    "name": "Chamois Niortais FC",
+    "country": "France",
+    "transfermarktUrl": "https://www.transfermarkt.com/chamois-niortais-fc/startseite/verein/280"
+  },
+  {
+    "name": "Grenoble Foot 38",
+    "country": "France",
+    "transfermarktUrl": "https://www.transfermarkt.com/grenoble-foot-38/startseite/verein/161"
+  },
+  {
+    "name": "AC Ajaccio",
+    "country": "France",
+    "transfermarktUrl": "https://www.transfermarkt.com/ac-ajaccio/startseite/verein/144"
+  },
+  {
+    "name": "Amiens SC",
+    "country": "France",
+    "transfermarktUrl": "https://www.transfermarkt.com/amiens-sc/startseite/verein/541"
+  },
+  {
+    "name": "Valenciennes FC",
+    "country": "France",
+    "transfermarktUrl": "https://www.transfermarkt.com/valenciennes-fc/startseite/verein/694"
+  },
+  {
+    "name": "USL Dunkerque",
+    "country": "France",
+    "transfermarktUrl": "https://www.transfermarkt.com/usl-dunkerque/startseite/verein/800"
+  },
+  {
+    "name": "EA Guingamp",
+   
+::contentReference[oaicite:0]{index=0}
+ 
